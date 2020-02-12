cb82da39b027f01337ea4631c5f4090dd4667eb6
Praveen B <praveen.bhaskar@hidubai.com>
2020-02-12 10:41:44 +0400

    #Integrate flutter module into Android

---
+++ HelloWorld_androidApp/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "flutter.native/helper"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Instantiate a FlutterEngine.
+        setContentView(R.layout.activity_main)
+
+        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // Note: this method is invoked on the main thread.
+                call, result ->
+            if (call.method == "onBackPressed") {
+                onBackPressed()
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        FlutterEngineCache
+            .getInstance()
+            .put("FlutterModule", flutterEngine)
+
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+    fun onClickButtonAction(view: View) {
+        startActivity(
+            withCachedEngine("FlutterModule")
+                .build(this)
+        )
+    }
+
+}
